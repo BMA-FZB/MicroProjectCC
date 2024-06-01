3aa0b5e85b573d6f3b2d09f90c27e12549e75534
BMA-FZB <mbouchaerb@gmail.com>
2024-06-02 00:47:29 +0100

    do some changes

--- app/routes.py
+++ app/routes.py
@@ -20,44 +20,44 @@ class create_embedded_pdf(Resource):
             base64_content = base64.b64encode(pdf_content)
             base64_string = base64_content.decode('utf-8')
             
-            # Construct dictionary with title and content
+
             base64_pdfs.append({
                 "title": file.filename,
                 "content": base64_string
             })
 
-        # Construct response data
+
         data = {'base64_pdfs': base64_pdfs}
 
-        # Return the data as JSON response
+
         return jsonify(data)
     
 
 class extract_embedded_pdf(Resource):
     def post(self):
-        # Read the embedded.json file
+
         json_data = request.files['embedded_json'].read()
 
-        # Parse JSON data
+
         data = json.loads(json_data)
 
-        # Initialize a BytesIO object to hold the ZIP file in memory
+
         zip_buffer = io.BytesIO()
 
-        # Create a ZIP file and add the PDF files to it
+  
         with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
-            # Loop through each PDF data in the JSON
+
             for pdf_data in data['base64_pdfs']:
-                # Decode base64 content
+
                 decoded_content = base64.b64decode(pdf_data['content'])
 
-                # Write PDF file to the ZIP file
+
                 zip_file.writestr(pdf_data['title'], decoded_content)
 
-        # Set the buffer's position to the beginning
+
         zip_buffer.seek(0)
 
-        # Return the ZIP file as a response
+
         return Response(
             zip_buffer,
             mimetype='application/zip',
